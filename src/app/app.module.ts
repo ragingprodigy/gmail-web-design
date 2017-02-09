02aa7fa25a6678d34eea790defa2c8199eb32eb1
Omonayajo Oladapo <o.omonayajo@gmail.com>
2017-02-09 22:44:20 +0100

    Added a router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,23 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
+
+const appRoutes: Routes = [
+  // { path: 'crisis-center', component: CrisisListComponent },
+  // { path: 'heroes', component: HeroListComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule
